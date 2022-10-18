98a4e6d196264226a99acddce14f17572a6ff0f5
FrederikGielis <frederik.gielis@student.thomasmore.be>
2022-10-18 14:44:27 +0200

    uploaded image display works

--- bricks/settings.py
+++ bricks/settings.py
@@ -127,5 +127,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # save image to this folder
 # DEFAULT_FILE_STORAGE = "/bricks/voting/images"
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'voting/media')
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'voting/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- voting/urls.py
+++ voting/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'voting'  # here for namespacing of urls.
 
@@ -18,3 +19,10 @@ urlpatterns = [
     path("dashboard/", views.dashboard, name="dashboard"),
     path("dashboard/addcreation/", views.addCreation, name="addcreation"),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
