c405518731cff5da5256a194fabfeff5629ae839
FrederikGielis <r0654052@student.thomasmore.be>
2022-11-07 18:59:47 +0100

    styling buttons

--- voting/views.py
+++ voting/views.py
@@ -23,26 +23,26 @@ def votingHomepage(request):
                   )
 
 
-def register(request):
-    if request.method == "POST":
-        print("POST")
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f"New account created: {username}")
-            login(request, user)
-            # TODO: login and route to myvotes
-            # return redirect("voting:votingHomepage")
-            return redirect("voting:myvotes")
+# def register(request):
+#     if request.method == "POST":
+#         print("POST")
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             username = form.cleaned_data.get('username')
+#             messages.success(request, f"New account created: {username}")
+#             login(request, user)
+#             # TODO: login and route to myvotes
+#             # return redirect("voting:votingHomepage")
+#             return redirect("voting:myvotes")
 
-        else:
-            for msg in form.error_messages:
-                messages.error(request, f"{msg}: {form.error_messages[msg]}")
+#         else:
+#             for msg in form.error_messages:
+#                 messages.error(request, f"{msg}: {form.error_messages[msg]}")
 
-            return render(request=request,
-                          template_name="voting/register.html",
-                          context={"form": form})
+#             return render(request=request,
+#                           template_name="voting/register.html",
+#                           context={"form": form})
 
     # elif request.method == "GET":
     form = UserCreationForm
