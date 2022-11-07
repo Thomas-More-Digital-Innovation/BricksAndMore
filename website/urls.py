74e6c0fe7b2e537375c350734d272e2fb8a4e9bc
FrederikGielis <r0654052@student.thomasmore.be>
2022-11-07 19:19:35 +0100

    push before chartJS

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,14 @@
+from allauth.account.views import LoginView, SignupView
 from django.urls import path, include
 from . import views
+from voting import views as voting_views
 
 
 app_name = 'website'  # here for namespacing of urls.
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
+    # Bypass the Django allauth login view and use the custom one
+    path('accounts/login/', voting_views.login_request, name="custom_login"),
+    path('accounts/signup/', voting_views.login_request, name="custom_singup"),
 ]
